bc84c54bc07d51ce4b9c135ac23877fef888aa59
samarth <samarthkulkarni@icloud.com>
2024-07-04 22:03:51 +0530

    bitonic sort for cpu

--- src/bitonic_sort.cu
+++ src/bitonic_sort.cu
@@ -9,3 +9,26 @@
 #include <fstream>
 
 #define MAX_THREADS_PER_BLOCK 1024
+
+void bitonicSortCPU(int* arr, int n) 
+{
+    for (int k = 2; k <= n; k *= 2) {
+        for (int j = k / 2; j > 0; j /= 2) {
+            for (int i = 0; i < n; ++i) {
+                int ij = i ^ j;
+
+                if (ij > i) {
+                    if ((i & k) == 0) {
+                        if (arr[i] > arr[ij]){
+                            std::swap(arr[i], arr[ij]);
+                        }
+                    } else {
+                        if (arr[i] < arr[ij]){
+                            std::swap(arr[i], arr[ij]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
